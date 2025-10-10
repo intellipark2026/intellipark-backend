554ec6245a6e1a64ef945658f3612ca058b41414
Patrick Guevarra <intellipark2026@gmail.com>
2025-10-10 16:57:49 +0800

    Use environment variables for Firebase credentials

--- index.js
+++ index.js
@@ -19,10 +19,13 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // Firebase Admin setup
-const serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://intellipark2025-327e9-default-rtdb.firebaseio.com"
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 const db = admin.database();
 
